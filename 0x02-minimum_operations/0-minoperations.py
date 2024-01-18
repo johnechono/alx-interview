cf5aeb78334f791fccdc8e66841a373eb074794d
johnechono <johninalegwu22@gmail.com>
2024-01-18 19:22:48 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,34 +1,17 @@
 #!/usr/bin/python3
-
-""" Lockboxes """
+"""A method that determines if all the boxes can be opened."""
 
 
 def canUnlockAll(boxes):
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
     """
-    - boxes is a list of lists
-    - A key with the same number as a box opens that box
-    - You can assume all keys will be positive integers
-    - The first box boxes[0] is unlocked
-    - Return True if all boxes can be opened, else return False
-    """
-    canUnlockAll = False
-    keys = {0: True}
-    n_boxes = len(boxes)
-    while (True):
 
-        n_keys = len(keys)
-
-        for i in range(len(boxes)):
-            if boxes[i] and keys.get(i, False):
-                for j in boxes[i]:
-                    if j < n_boxes:
-                        keys[j] = True
-                    boxes[i] = None
-
-        if not (len(keys) > n_keys):
-            break
-
-    if n_keys == len(boxes):
-        canUnlockAll = True
-
-    return canUnlockAll
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
